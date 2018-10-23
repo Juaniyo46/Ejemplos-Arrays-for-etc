f57b1009fffc9b33638666aea57f6d9116d19ea1
Juan <juan.alvarez@escuelaestech.es>
2018-10-23 11:52:09 +0200

    Ejemplo multiplicar dos arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,40 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
 
-    public static int sumaArray (int [] array) {
-        int suma = 0;
-        for (int n: array) {
-            suma +=n;
-        }
-        return suma;
-    }
-
     public static void main(String[] args) {
 
-        int [] a = {1,2,3,4,5,6};
+        Scanner sc = new Scanner(System.in);
 
-        int sumaA = sumaArray(a);
-        System.out.println("La suma del array a es: "+sumaA);
+        //Tamaño del array
+        System.out.println("Introduzca el tamaño de los arrays (Número entero)");
+        int tamano = sc.nextInt();
+
+        //Creamos los dos arrays pedidos vacios
+        int[] arrayPrimero=createArray(tamano,sc);
+        int[] arraySegundo=createArray(tamano,sc);
+
+
+        //Creamos bucle de multiplicación
+        int[] arrayResultado=new int[tamano];
+        for (int i =0; i<tamano;i++) {
+            arrayResultado[i]=arrayPrimero[i]*arraySegundo[i];
+        }
+
+        System.out.println("El array primero " + Arrays.toString(arrayPrimero)+" por el array segundo "+Arrays.toString(arraySegundo)+"es igual a: "+Arrays.toString(arrayResultado));
 
     }
+    public static int[] createArray (int tamano, Scanner sc) {
+        //Creamos un array nuevo vacio
+        System.out.printf("Introduzca %d valores separados por espacios",tamano);
+        int [] array = new int[tamano];
+            for (int i=0; i<tamano;i++) {
+                array[i]=sc.nextInt();
+        } return array;
+    }
+
 }
